c6754a6701c39162ad9dc29c3c303e536fecc7bf
Howie YU <howie.yu@gmail.com>
2018-12-04 11:45:28 +0800

    Update customer image with gpyOpt

--- custom-images/create_image.sh
+++ custom-images/create_image.sh
@@ -2,10 +2,11 @@
 # https://cloud.google.com/dataproc/docs/release-notes
 
 python generate_custom_image.py \
-  --image-name pyspark-with-conda-v2 \
+  --image-name pyspark-with-conda-v2-365 \
   --dataproc-version 1.3.5-deb9 \
   --customization-script customization_script.sh \
   --daisy-path ~/bin/daisy \
   --zone asia-east1-b \
-  --gcs-bucket gs://pvmax-spark-image
+  --gcs-bucket gs://pvmax-spark-image \
+  --expire-day 365
 
